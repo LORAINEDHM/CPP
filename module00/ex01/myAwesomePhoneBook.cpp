2601a78b2830096dd885e485344284144e9d7731
Loraine Duhamel <lduhamel@fu-r6-p4.s19.be>
2021-09-30 18:32:30 +0200

    ex01 brouillon

---
+++ module00/ex01/myAwesomePhoneBook.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   myAwesomePhoneBook.cpp                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lduhamel <lduhamel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/30 17:14:40 by lduhamel          #+#    #+#             */
+/*   Updated: 2021/09/30 17:18:59 by lduhamel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "myAwesomePhoneBook.hpp"
+
+Search::Search() {
+
+	std::cout << "Constructor called" << std::endl;
+	return;
+}
+
+Search::~Search() {
+
+	std::cout << "Destructor called" << std::endl;
+	return;
+}
+
---
+++ module00/ex01/myAwesomePhoneBook.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   myAwesomePhoneBook.hpp                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lduhamel <lduhamel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/30 17:07:51 by lduhamel          #+#    #+#             */
+/*   Updated: 2021/09/30 17:14:37 by lduhamel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MY_AWESOME_PHONE_BOOK_H
+# define MY_AWESOME_PHONE_BOOK_H
+
+class Search {
+
+	public:
+	
+		Search();
+		~Search(void);
+};
+
+class Add {
+
+}
+
+class Exit
+
+
+
+#endif
