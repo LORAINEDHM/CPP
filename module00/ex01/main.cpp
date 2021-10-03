bfbcb0487c0ceba792f99db5aace2ba9dc9dbe00
loraineduhamel <loraineduhamel@gmail.com>
2021-10-03 18:09:50 +0200

    ex01 ok

--- module00/ex01/main.cpp
+++ module00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lduhamel <lduhamel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/02 13:27:42 by lduhamel          #+#    #+#             */
-/*   Updated: 2021/10/03 18:00:56 by lduhamel         ###   ########.fr       */
+/*   Updated: 2021/10/03 18:09:25 by lduhamel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@
 
 int		main(void) {
 
-    char 	cmd[100];
-	PhoneBook phonebook;
+	std::string		cmd;
+	PhoneBook		phonebook;
 
 	phonebook.nb = 0;
     while (phonebook.nb >= 0)
     {
         std::cout << "Enter one of the following commands :\n- ADD\n- SEARCH \n- EXIT" << std::endl;
         std::cin >> cmd;
-        if (std::strcmp(cmd, "ADD") == 0)
+		if (cmd.compare("ADD") == 0)
 			phonebook.addContact();
-        else if (std::strcmp(cmd, "SEARCH") == 0)
+		else if (cmd.compare("SEARCH") == 0)
 				phonebook.searchContact();
-        else if (std::strcmp(cmd, "EXIT") == 0)
+		else if (cmd.compare("EXIT") == 0)
             break;
     }
 }
