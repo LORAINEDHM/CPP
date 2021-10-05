913bdfbb80107d16145c866a05ce83e9c002a8da
Loraine Duhamel <lduhamel@fu-r9-p5.s19.be>
2021-10-05 13:44:59 +0200

    module01 ex00 and ex01 ok

---
+++ module01/ex01/Zombie.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lduhamel <lduhamel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/04 16:43:31 by lduhamel          #+#    #+#             */
+/*   Updated: 2021/10/05 13:42:52 by lduhamel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie( std::string name ) : _name(name) { }
+
+Zombie::Zombie( void ) { }
+
+Zombie::~Zombie( void ) {
+
+	std::cout << this->_name << " destructor called" << std::endl;
+}
+
+void	Zombie::announce( void ) const {
+
+	std::cout << this->_name << " BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+
