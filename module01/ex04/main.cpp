1d054c048f172f0a82f4b36a18483d869350b1a2
Loraine Duhamel <lduhamel@fu-r1-p5.s19.be>
2021-10-06 19:40:22 +0200

    ex04 in progress:

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -6,12 +6,15 @@
 /*   By: lduhamel <lduhamel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 17:11:04 by lduhamel          #+#    #+#             */
-/*   Updated: 2021/10/06 14:51:52 by lduhamel         ###   ########.fr       */
+/*   Updated: 2021/10/06 19:01:24 by lduhamel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <streambuf>
+#include <string>
+//#include <sstream>
 
 int		handleError( const char *str ) {
 	
@@ -29,24 +32,44 @@ int		parsing( int ac, char **av ) {
 void	fillFile( std::string filename, std::string s1, std::string s2, std::ifstream& ifs ) {
 	
 	std::ofstream ofs(filename);
-	std::string line;
+	std::string str;
 	int pos;
-	int i = 0;
 
 	if (!ofs) {
     	std::cerr << "Error: file could not be opened" << std::endl;
     	exit(1);
 	}
-	while (getline(ifs, line)) {
-		if (i != 0)
-			ofs << std::endl;
-		if ((pos = line.find(s1)) != std::string::npos) {	
-			line.erase(pos, s1.length());
-			line.insert(pos, s2);
-		}
-		ofs << line;
-		i++;
+	str = std::string((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+	pos = str.find(s1);
+	while (pos != std::string::npos) {
+		std::cout << pos << std::endl;
+		str.erase(pos, s1.length());
+		str.insert(pos, s2);
+		std::cout << pos << std::endl;
+		pos = (&str[pos + s1.length()]).find(s1);
+		std::cout << pos << std::endl;
+		break;
+
+		//ofs << str << std::endl;
 	}
+	ofs << str;
+
+	// while (getline(ofs, line)) {
+	// 	// endflag = 0;
+	// 	// if (i != 0)
+	// 	// 	ofs << std::endl;
+	// 	if ((pos = line.find(s1)) != std::string::npos) {
+	// 		s1len = s1.length();
+	// 		// if (line[pos + s1len] == '\n')
+	// 		// 	endflag = 1;
+	// 		line.erase(pos, s1len);
+	// 		line.insert(pos, s2);
+	// 	}
+	// 	ofs << line << std::endl;
+	// 	i++;
+	// }
+	// if ( endflag == 1)
+	// 	ofs << std::endl;
 	ifs.close();
 	ofs.close();
 }
